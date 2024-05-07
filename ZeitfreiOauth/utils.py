aed22822dfdabd2bf58a5847eb324cd2f900237d
Nat1anWasTaken <nathan@nat1an.xyz>
2024-05-07 22:39:31 +0800

    fix(ZeitfreiOauth/utils.py): update datetime format in parse_datetime function

--- ZeitfreiOauth/utils.py
+++ ZeitfreiOauth/utils.py
@@ -4,8 +4,8 @@ from datetime import datetime
 def parse_datetime(datetime_string: str) -> datetime:
     """
     Parse datetime string to datetime object
-    The string format should be "%Y-%m-%dT%H:%M:%S.%fZ"
+    The string format should be "%Y-%m-%dT%H:%M:%S.%f"
     :param datetime_string: datetime string
     :return: datetime object
     """
-    return datetime.strptime(datetime_string, "%Y-%m-%dT%H:%M:%S.%fZ")
+    return datetime.strptime(datetime_string, "%Y-%m-%d %H:%M:%S.%f")
