27abcea988bca0f610a52965049b221708b375c7
Nat1anWasTaken <nathan@nat1an.xyz>
2024-05-07 22:21:21 +0800

    fix(ZeitfreiOauth/__init__.py): corrected import path for DiscordOAuthClient

--- ZeitfreiOauth/__init__.py
+++ ZeitfreiOauth/__init__.py
@@ -3,7 +3,7 @@
 """
 
 from .async_client import AsyncDiscordOAuthClient
-from ZeitfreiOauth.old.client import DiscordOAuthClient
+from .old.client import DiscordOAuthClient
 from .exceptions import UserIsUnauthorized, UserIsAlreadyIn
 from .models import User, Guild, ResultResponse
 from .utils import parse_datetime
